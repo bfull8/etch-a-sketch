761b3c4c7dce3b35ebe4a75fa53ea8586818d6e5
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-08-12 22:18:46 -0400

    created a 16x16 grid of squares using empty divs

---
+++ es.js
@@ -0,0 +1,7 @@
+// Create 16x16 grid using flexbox
+const container = document.querySelector("#container");
+for (let i = 0; i < 16 * 16; i++) {
+  const square = document.createElement("div");
+  square.classList = "square";
+  container.appendChild(square);
+}
