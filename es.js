6912254c61a68dc9ba965f2a1a9f945b917a312b
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-08-12 22:50:44 -0400

    refactored to make createGrid a function instead of repeating arrow functions

--- es.js
+++ es.js
@@ -1,43 +1,33 @@
-// Create 16x16 grid using flexbox
-const container = document.querySelector("#container");
-for (let i = 0; i < 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.classList = "square";
-  square.style.flex = "1 1 calc(100% / 16)";
-  container.appendChild(square);
-
-  // Add a background color hovering over a square
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "green";
-  });
-}
-
-// Prompt user how large the grid should be
-const gridSizeBtn = document.querySelector("#grid-size-btn");
-gridSizeBtn.addEventListener("click", () => {
-  let gridSize;
-  while (true) {
-    gridSize = prompt("How many squares in each row/column would you like?");
-    if (gridSize < 1 || gridSize > 100) {
-      continue;
-    } else {
-      break;
-    }
-  }
-
-  // Reset the grid
+function createGrid(gridSize) {
   container.innerHTML = "";
 
-  // readd the squares
   for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement("div");
     square.classList = "square";
     square.style.flex = `1 1 calc(100% / ${gridSize})`;
-    container.appendChild(square);
 
-    // Add a background color hovering over a square
     square.addEventListener("mouseenter", () => {
       square.style.backgroundColor = "green";
     });
+
+    container.appendChild(square);
   }
+}
+
+const container = document.querySelector("#container");
+const gridSizeBtn = document.querySelector("#grid-size-btn");
+
+// Create 16x16 grid using flexbox
+createGrid(16);
+
+// Allow user to change grid size
+gridSizeBtn.addEventListener("click", () => {
+  let gridSize;
+  while (true) {
+    gridSize = prompt("How many squares in each row/column would you like?");
+    if (gridSize >= 1 && gridSize <= 100) {
+      break;
+    }
+  }
+  createGrid(gridSize);
 });
