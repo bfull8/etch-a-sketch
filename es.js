ad559270fd6c1cca3ccf74a806381865e7579205
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-08-12 22:25:08 -0400

    added mousenter eventlistener to highlight square green and leave a trail

--- es.js
+++ es.js
@@ -4,4 +4,9 @@ for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
   square.classList = "square";
   container.appendChild(square);
+
+  // Add a background color hovering over a square
+  square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor = "green";
+  });
 }
